d5ec793d3c1d98b7b028c718da31b2bac458052f
claraikik998 <claraikik998@gmail.com>
2026-06-21 23:56:04 -0300

    Little improvement in board printing

--- src/application/UI.java
+++ src/application/UI.java
@@ -1,9 +1,33 @@
 package application;
 
-import chess.chesspiece; // O VS Code deve colocar esse import automaticamente
+import chess.Color;
+import chess.chesspiece; 
 
 public class UI {
     
+// retirado de https://stackoverflow.com/questions/5762491/how-to-print-color-in-console-using-system-out-println
+
+public static final String ANSI_RESET = "\u001B[0m";
+public static final String ANSI_BLACK = "\u001B[30m";
+public static final String ANSI_RED = "\u001B[31m";
+public static final String ANSI_GREEN = "\u001B[32m";
+public static final String ANSI_YELLOW = "\u001B[33m";
+public static final String ANSI_BLUE = "\u001B[34m";
+public static final String ANSI_PURPLE = "\u001B[35m";
+public static final String ANSI_CYAN = "\u001B[36m";
+public static final String ANSI_WHITE = "\u001B[37m";
+public static final String ANSI_PINK = "\u001B[38;5;205m";  // cor rosa
+
+public static final String ANSI_BLACK_BACKGROUND = "\u001B[40m";
+public static final String ANSI_RED_BACKGROUND = "\u001B[41m";
+public static final String ANSI_GREEN_BACKGROUND = "\u001B[42m";
+public static final String ANSI_YELLOW_BACKGROUND = "\u001B[43m";
+public static final String ANSI_BLUE_BACKGROUND = "\u001B[44m";
+public static final String ANSI_PURPLE_BACKGROUND = "\u001B[45m";
+public static final String ANSI_CYAN_BACKGROUND = "\u001B[46m";
+public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
+public static final String ANSI_PINK_BACKGROUND = "\u001B[48;5;205m"; // cor rosa de fundo
+
     public static void printBoard(chesspiece[][] pieces) {
         // parte do desenho do tabuleiro
 
@@ -17,13 +41,21 @@ public class UI {
         System.out.println("  a b c d e f g h");
     } 
 
-    private static void printPiece(chesspiece piece) {
-        // imprimir uma unica peça
+    private static void printPiece(chesspiece piece) { // ou ChessPiece dependendo de como você escreveu
         if (piece == null) {
             System.out.print("-");
-        } else {
-            System.out.print(piece);
         }
-        System.out.print(" "); 
+        else {
+            if (piece.getColor() == Color.WHITE) {
+                System.out.print(ANSI_WHITE + piece + ANSI_RESET);
+            }
+            else {
+                // Troquei ANSI_BLACK por ANSI_PINK para as peças ficarem rosas já que o terminal é preto
+                System.out.print(ANSI_PINK + piece + ANSI_RESET); 
+            }
+        }
+        System.out.print(" ");
     }
+
+
 }
--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -39,10 +39,19 @@ private void placeNewPiece(char column, int row, chesspiece piece) {
   private void initialSetup() {
     //colocar as peças no tabuleiro
    
-    placeNewPiece('b', 6, new Rook(board, Color.WHITE));
-    placeNewPiece('e', 8, new king(board, Color.BLACK));
-    placeNewPiece('e', 1, new king(board, Color.WHITE));
+        placeNewPiece('c', 1, new Rook(board, Color.WHITE));
+        placeNewPiece('c', 2, new Rook(board, Color.WHITE));
+        placeNewPiece('d', 2, new Rook(board, Color.WHITE));
+        placeNewPiece('e', 2, new Rook(board, Color.WHITE));
+        placeNewPiece('e', 1, new Rook(board, Color.WHITE));
+        placeNewPiece('d', 1, new king(board, Color.WHITE));
 
+        placeNewPiece('c', 7, new Rook(board, Color.BLACK));
+        placeNewPiece('c', 8, new Rook(board, Color.BLACK));
+        placeNewPiece('d', 7, new Rook(board, Color.BLACK));
+        placeNewPiece('e', 7, new Rook(board, Color.BLACK));
+        placeNewPiece('e', 8, new Rook(board, Color.BLACK));
+        placeNewPiece('d', 8, new king(board, Color.BLACK));
   }
 
 }
